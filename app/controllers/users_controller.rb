449adf80c22083465bc38214877f1cb06d02c6ff
Paulina <paulina.recko@gmail.com>
2019-06-25 13:28:03 +0200

    Enable adding users to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-  @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
-
+      redirect_to @user
     else
       render 'new'
     end
